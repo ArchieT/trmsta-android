7e9420868ae5aa967be9c6755ad8414aa5df1add
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-09-04 23:04:50 +0200

    Download async improvements, v0.0.2

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,14 @@ import "package:flutter/http.dart" as http;
 import "dart:async";
 
 void main() {
-  runApp(
-    new MaterialApp(
+  runApp(new MaterialApp(
       title: 'TRMSta ListNow',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: new TrmStaListNow()
-    )
-  );
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new TrmStaListNow()));
 }
 
 class TrmStaListNow extends StatefulWidget {
-  TrmStaListNow({ Key key }) : super(key: key);
+  TrmStaListNow({Key key}) : super(key: key);
 
   @override
   _TrmStaListNowState createState() => new _TrmStaListNowState();
@@ -26,33 +21,28 @@ class _TrmStaListNowState extends State<TrmStaListNow> {
   List<AllSta> _lista;
   DateTime _when;
 
-  void _download() {
-    setState(() {
-        Future<Downloaded> ourdownloaded() async {
-          return await download((String url) => http.read(url));
-        }
-        ourdownloaded().then((Downloaded down) {
-          _when = down.time;
-          _lista = down.ParseAll();
-        });
+  void _download() async {
+    download((String url) => http.read(url)).then((Downloaded down) {
+      setState(() {
+        _when = down.time;
+        _lista = down.ParseAll();
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Stations ${_when.toString()}')
-      ),
-      body: new ScrollableList(
-        children: _lista==null?[]:_lista.map((AllSta our) => new Text(our.toString(), key: new ValueKey<int>(our.locrow.stanum))),
-        itemExtent: 50.0
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _download,
-        tooltip: 'Download',
-        child: new Icon(Icons.refresh)
-      )
-    );
+        appBar: new AppBar(title: new Text('Stations ${_when.toString()}')),
+        body: new ScrollableList(
+            children: _lista == null
+                ? []
+                : _lista.map((AllSta our) => new Text(our.toString(),
+                    key: new ValueKey<int>(our.locrow.stanum))),
+            itemExtent: 50.0),
+        floatingActionButton: new FloatingActionButton(
+            onPressed: _download,
+            tooltip: 'Download',
+            child: new Icon(Icons.refresh)));
   }
 }
