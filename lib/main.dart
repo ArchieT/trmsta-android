580fb831447bca065083838afec2c3c0be3112ba
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-09-04 19:46:34 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import "package:trmsta_nohttp/trmsta.dart";
+import "package:flutter/http.dart" as http;
+import "dart:async";
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'TRMSta ListNow',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: new TrmStaListNow()
+    )
+  );
+}
+
+class TrmStaListNow extends StatefulWidget {
+  TrmStaListNow({ Key key }) : super(key: key);
+
+  @override
+  _TrmStaListNowState createState() => new _TrmStaListNowState();
+}
+
+class _TrmStaListNowState extends State<TrmStaListNow> {
+  List<AllSta> _lista;
+  DateTime _when;
+
+  void _download() {
+    setState(() {
+        Future<Downloaded> ourdownloaded() async {
+          return await download((String url) => http.read(url));
+        }
+        ourdownloaded().then((Downloaded down) {
+          _when = down.time;
+          _lista = down.ParseAll();
+        });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Stations ${_when.toString()}')
+      ),
+      body: new ScrollableList(
+        children: _lista==null?[]:_lista.map((AllSta our) => new Text(our.toString(), key: new ValueKey<int>(our.locrow.stanum))),
+        itemExtent: 50.0
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _download,
+        tooltip: 'Download',
+        child: new Icon(Icons.refresh)
+      )
+    );
+  }
+}
