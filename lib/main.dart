d479968bcbab0397bd3be085577a772f333e1b55
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2016-09-05 20:48:19 +0200

    Working on refreshing and UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import "package:trmsta_nohttp/trmsta.dart";
 import "package:flutter/http.dart" as http;
-import "dart:async";
+//import "dart:async";
 
 void main() {
   runApp(new MaterialApp(
@@ -19,12 +19,20 @@ class TrmStaListNow extends StatefulWidget {
 
 class _TrmStaListNowState extends State<TrmStaListNow> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  /*
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
       new GlobalKey<RefreshIndicatorState>();
+      */
   static final GlobalKey<ScrollableState> _scrollableKey =
       new GlobalKey<ScrollableState>();
   List<AllSta> _lista;
   DateTime _when;
+
+  @override
+  void initState() {
+    super.initState();
+    _download();
+  }
 
   void _download() {
     download((String url) => http.read(url)).then((Downloaded down) {
@@ -35,17 +43,19 @@ class _TrmStaListNowState extends State<TrmStaListNow> {
     });
   }
 
+/*
   Future<Null> _refresh() async {
     Completer<Null> completer = new Completer<Null>();
     _download();
     return completer.future.then((_) {});
   }
+  */
 
   @override
   Widget build(BuildContext context) {
     Widget body = new ScrollConfiguration(child: new MaterialList(
         type: MaterialListType.threeLine,
-        padding: const EdgeInsets.all(8.0),
+        //padding: const EdgeInsets.all(8.0),
         scrollableKey: _scrollableKey,
         children: (_lista == null
             ? new List<ListItem>()
@@ -54,23 +64,30 @@ class _TrmStaListNowState extends State<TrmStaListNow> {
                     isThreeLine: true,
                     leading: new CircleAvatar(
                         child: new Text(item.locrow.stanum.toString())),
-                    title: new Text(item.data.addr),
-                    subtitle: new Text(item.locrow.row.toString()));
+                    title: new Text(
+                        item.data.addr + " " + item.locrow.row.toString()),
+                    subtitle: new Text("█" * item.locrow.row.dostrow +
+                        "▒" * item.locrow.row.wolrow));
               })))));
+    /*
     body = new RefreshIndicator(
         key: _refreshIndicatorKey,
         child: body,
         refresh: _refresh,
         scrollableKey: _scrollableKey,
         location: RefreshIndicatorLocation.top);
+        */
     return new Scaffold(
         key: _scaffoldKey,
-        scrollableKey: _scrollableKey,
-        appBar: new AppBar(title: new Text('Stations ${_when.toString()}')),
-        body: body,
-        floatingActionButton: new FloatingActionButton(
-            onPressed: _download,
-            tooltip: 'Download',
-            child: new Icon(Icons.refresh)));
+        //scrollableKey: _scrollableKey,
+        appBar: new AppBar(
+            title: new Text('Stations ${_when.toString()}'),
+            actions: <Widget>[
+              new IconButton(
+                  onPressed: _download,
+                  tooltip: 'Download',
+                  icon: new Icon(Icons.refresh))
+            ]),
+        body: body);
   }
 }
